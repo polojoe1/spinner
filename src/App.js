8ef0c029d7912a3fd167346ae0fa256e9ce3e38e
Joseph Coats <coats.joseph30@gmail.com>
2023-01-26 22:31:58 -0600

    solved bug!!

--- src/App.js
+++ src/App.js
@@ -13,20 +13,15 @@ function App() {
 
   return (
     <div className='flex  justify-center bg-gray-700 h-screen w-screen  pt-10' >
-      {large?
-      <div id='drop' style={{height:'92vh'}} className=' w-[25rem] origin-top duration-500 rounded-2xl bg-gradient-to-r flex justify-center items-center from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
-      <div id='drop2' className='h-[90vh] w-96 bg-slate-900 origin-top duration-500 flex p-5 rounded-xl items-center flex-col'>
+      
+      <div id='drop' className={`${large?'h-[92vh]':'h-[32vh]'} w-[25rem] duration-500 rounded-2xl bg-gradient-to-r flex justify-center items-center from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] `}>
+      <div id='drop2' className={`${large?'h-[90vh]':'h-[30vh]'} w-96 bg-slate-900 duration-500 flex p-5 rounded-xl items-center flex-col`}>
         
         <p id='demo' className='text-white text-5xl text-center pt-6'></p>
-        <div  onClick={()=>{setLarge(!large)}} className='h-12  origin-bottom duration-500 w-12 rounded-full bg-gradient-to-r from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] flex justify-center items-center mt-[30rem]  text-white text-2xl font-bold cursor-pointer'>↑</div>
+        <p className={`${large?'':'hidden'} text-white`}>+</p>
+        <div  onClick={()=>{setLarge(!large)}} className={`${large?'mt-[20rem]':'rotate-180 mt-5'} h-12  duration-500 w-12 rounded-full bg-gradient-to-r from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] flex justify-center items-center  text-white text-2xl font-bold cursor-pointer`}>↑</div>
         </div>
-      </div>:
-      <div id='drop' style={{height:'32vh'}} className='flex origin-bottom duration-500 justify-center items-center rounded-2xl  w-[25rem] bg-gradient-to-r from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
-      <div id='drop2' className='h-[30vh] w-96 origin-bottom  duration-500 bg-slate-900 flex p-5 rounded-xl flex-col items-center '>
-        <p id='demo' className='text-white text-5xl text-center pt-6'></p>
-        <div  onClick={()=>{setLarge(!large)}} className='h-12 w-12 translate-y-2 rounded-full bg-gradient-to-r from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] flex justify-center items-center mt-12 text-white text-2xl font-bold cursor-pointer origin-bottom duration-500'> ↓</div>
-    </div></div>}
-      
+      </div>
     </div>
   );
 }
