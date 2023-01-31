6e72ea2d73be71e92969d7c9c8491c9e61d94b0a
Joseph Coats <coats.joseph30@gmail.com>
2023-01-30 21:59:25 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ function App() {
   
   const setDate =()=>{
     let d = new Date();
+    if(loaded===false){
+      setLoaded(true)
+    }
     document.getElementById('demo').innerHTML = d.toLocaleTimeString()
     
   }
   setInterval(setDate,1000)
+  
+
+  
   const appear=()=>{
     setLarge(!large)
   }
@@ -41,13 +47,14 @@ function App() {
     //AM PM logic
     amOrPm =storedTimeAsStringVar[storedTimeAsStringVar.length-2] +storedTimeAsStringVar[storedTimeAsStringVar.length-1]
     }
+    
   return (
-    <div onLoad={()=>setLoaded(true)} className='flex  justify-center bg-gray-700 h-screen w-screen  pt-10' >
+    <div   className='flex  justify-center bg-gray-700 h-screen w-screen  pt-10' >
       
-      <div id='drop'  className={`${large?`${setLoaded(true)} h-[92vh]`:'h-[32vh]'} w-[25rem] duration-500 rounded-2xl bg-gradient-to-r flex justify-center items-center from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] `}>
+      <div id='drop' className={`${large?` h-[92vh]`:'h-[32vh]'} w-[25rem] duration-500 rounded-2xl bg-gradient-to-r flex justify-center items-center from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] `}>
       <div id='drop2' className={`${large?'h-[90vh]':'h-[30vh]'} w-96 bg-slate-900  duration-500 flex p-5 rounded-xl items-center flex-col justify-between`}>
         
-        <div><p id='demo' className='text-white text-5xl text-center pt-6'></p>
+        <div><p id='demo' className={` text-white text-5xl text-center pt-6`}></p>
         <div id='appear' className={`${large?'h-[92vh]:hidden':'hidden'}`}><div className='overflow-scroll flex flex-col '><p className={`${large?'':'hidden'} text-green-300  text-3xl pt-[2rem] pl-[20rem] origin-right cursor-pointer duration-500 mt-[1rem] pb-3`} onClick={()=>setAddAlarm(!addAlarm)}>+</p>
           {alarms.map(alarm=>{return (
             <div key={alarm.id} className={` ${large?' delay-75 flex':'hidden'} w-[350px] transition-opacity duration-500 rounded-2xl bg-slate-800 h-28 flex items-center justify-around`}>
@@ -92,7 +99,7 @@ function App() {
           <p className='text-white mr-8 text-6xl cursor-pointer rotate-180'>^</p>
           <p className='text-white text-6xl cursor-pointer rotate-180'>^</p>
         </div>
-      </div>:'':''}
+      </div>:'':''} 
     </div>
   );
 }
