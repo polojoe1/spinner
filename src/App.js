a59e87f7960a3ddb797225212d9ce70a3891eb77
Joseph Coats <coats.joseph30@gmail.com>
2023-02-02 19:15:42 -0600

    tweaks needed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState} from 'react'
 function App() {
   const [loaded,setLoaded]=useState(false)
   const [large, setLarge] =useState(true)
-  const [alarms,setAlarms]=useState([{id:1,time:'10:15:00 PM',on:true}])
+  const [alarms,setAlarms]=useState([{id:0,time:'10:15:00 PM',on:true}])
   
   
   
@@ -37,6 +37,8 @@ function App() {
   const [amOrPm,setAmOrPm] =useState(storedTimeAsStringVar[storedTimeAsStringVar.length-2] +storedTimeAsStringVar[storedTimeAsStringVar.length-1])
     
     
+
+  //arrow up/down functions
     const arrowUpFunction = () =>{
       
       if(amOrPm==='PM'){
@@ -81,6 +83,20 @@ function App() {
         setSecondNumberToEdit((parseInt(secondNumberToEdit)-1).toString())
       }
     }
+
+
+
+    //submit form function
+
+
+    const saveAlarm = ()=>{
+      //{id:1,time:'10:15:00 PM',on:true}
+      setAlarms(
+        [...alarms,{id: alarms.length, on:true, time: firstNumberToEdit+':'+secondNumberToEdit+':00 '+amOrPm}]
+        )
+        setAddAlarm(!addAlarm)
+        console.log(...alarms)
+    }
   return (
     <div   className='flex  justify-center bg-gray-700 h-screen w-screen  pt-10' >
       
@@ -88,11 +104,11 @@ function App() {
       <div id='drop2' className={`${large?'h-[90vh]':'h-[30vh]'} w-96 bg-slate-900  duration-500 flex p-5 rounded-xl items-center flex-col justify-between`}>
         
         <div><p id='demo' className={` text-white text-5xl text-center pt-6`}>12:00:00 PM</p>
-        <div id='appear' className={`${large?'h-[92vh]:hidden':'hidden'}`}><div className='overflow-scroll flex flex-col '><p className={`${large?'':'hidden'} text-green-300  text-3xl pt-[2rem] pl-[20rem] origin-right cursor-pointer duration-500 mt-[1rem] pb-3`} onClick={()=>setAddAlarm(!addAlarm)}>+</p>
+        <div id='appear' className={`${large?'h-[92vh]:hidden':'hidden'}`}><div className=' h-[26rem] flex flex-col overflow-scroll'><p className={`${large?'':'hidden'} text-green-300  text-3xl pt-[2rem] pl-[20rem] origin-right cursor-pointer duration-500 mt-[1rem] pb-3`} onClick={()=>setAddAlarm(!addAlarm)}>+</p>
           {alarms.map(alarm=>{return (
-            <div key={alarm.id} className={` ${large?' delay-75 flex':'hidden'} w-[350px] transition-opacity duration-500 rounded-2xl bg-slate-800 h-28 flex items-center justify-around`}>
-              <p className={`text-white`}>{isNaN(alarm.time[4])?alarm.time.slice(0,4):alarm.time.slice(0,5)} {alarm.time[alarm.time.length-2]+alarm.time[alarm.time.length-1]}</p>
-              <div onClick={()=>setAlarms([{...alarms,id:alarm.id,time:alarm.time,on:!alarm.on}])} className={`${alarm.on?'justify-end bg-purple-400 border-[1px]  border-purple-800':'bg-slate-300'} duration-75 h-4 w-8 cursor-pointer rounded-full flex items-center `}>
+            <div key={alarm.id} className={` ${large?' delay-75 flex':'hidden'} w-[350px] transition-opacity duration-500 rounded-2xl bg-slate-800 h-[200px] items-center justify-around`}>
+              <p className={`text-white `}>{isNaN(alarm.time[4])?alarm.time.slice(0,4):alarm.time.slice(0,5)} {alarm.time[alarm.time.length-2]+alarm.time[alarm.time.length-1]}</p>
+              <div onClick={()=>setAlarms([...alarms,{id:alarm.id,time:alarm.time,on:!alarm.on}])} className={`${alarm.on?'justify-end bg-purple-400 border-[1px]  border-purple-800':'bg-slate-300'} duration-75 h-4 w-8 cursor-pointer rounded-full flex items-center `}>
                 <div className={` ${alarm.on?'border-[1px] border-purple-800 origin-left ':' origin-right'} duration-1000 h-5 w-5 rounded-full bg-white`}></div>
               </div>
             </div>
@@ -134,7 +150,7 @@ function App() {
         </div>
         <div className='flex mt-56 justify-around text-2xl text-orange-300'>
           <p onClick={()=>setAddAlarm(!addAlarm)} className='hover:bg-slate-600 rounded-3xl cursor-pointer w-32 text-center'>Cancel</p>
-          <p className='hover:bg-slate-600 rounded-3xl cursor-pointer w-32 text-center'>Save</p>
+          <p onClick={()=>saveAlarm()} className='hover:bg-slate-600 rounded-3xl cursor-pointer w-32 text-center'>Save</p>
         </div>
       </div>:'':''} 
     </div>
